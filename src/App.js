d820800874585e87fb41214e537a22f7efc029ca
FreyaP <65002708+FreyaP@users.noreply.github.com>
2020-08-02 22:19:02 -0700

    Added What We Do component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from "./Header";
 import models from "./images/models.png";
+import WhatWeDo from "./WhatWeDo";
 
 import './App.css';
 
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Header />
       <img src={models} alt="Tesla Model S" className="responsive" />
+      <WhatWeDo />
     </div>
   );
 }
---
+++ src/WhatWeDo.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./WhatWeDo.css";
+
+export default function WhatWeDo() {
+return (
+    <div className="WhatWeDo">
+<h1>Go Green, Ride Green. </h1>
+<ul>
+    <li>
+        Sustainable, zero emission travel around the Sea To Sky and British Columbia.
+    </li>
+    <li>
+        Your socially reposnsible option to help protect the environment you play in.
+    </li>
+    <li>
+    Customizable shuttle trips that can off set your travel.
+    </li>
+</ul>
+
+    </div>
+)}
